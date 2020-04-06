416dcfa7f8bbc8427180aa589f30005e7818cbf6
Björn Veit <dev@bjoernveit.com>
2020-04-06 21:34:36 +0200

    init: empty card table component

---
+++ src/components/card table/CardTable.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card-table">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+}
+</script>
+
+<style lang="stylus">
+</style>
