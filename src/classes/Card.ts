0dfba94607009f87902de178a766777628fb049e
Björn Veit <dev@bjoernveit.com>
2020-04-06 21:02:59 +0200

    feat value and color of card now private

--- src/classes/Card.ts
+++ src/classes/Card.ts
@@ -1,13 +1,21 @@
 import { RGBAColor } from './RGBAColors'
 export class Card {
     constructor(
-        public value: string,
-        public color: RGBAColor,
+        private value: string,
+        private color: RGBAColor,
         public state: CardState
     ){}
+
+    getValue(): string{
+        return this.value;
+    }
+
+    getColor(): RGBAColor{
+        return this.color;
+    }
 }
 
 // State object containing meta data of the state of on specific card
 export interface CardState{
-    //TODO: Stuff like position, orientation, active, heldByPlayer ... 
+    // TODO: Stuff like position, orientation, active, heldByPlayer ... 
 }
