16cb85df91d8d4836897e272d009e6e2f0f37af6
Björn Veit <dev@bjoernveit.com>
2020-04-07 12:31:58 +0200

    fixL renaming Card --> CardData

--- src/interfaces/Deck.ts
+++ src/interfaces/Deck.ts
@@ -1,10 +1,10 @@
-import { Card } from '../classes/Card'
+import { CardData } from "../classes/CardData";
 export interface Deck {
-    cards: Array<Card>;
+  cards: Array<CardData>;
 
-    // Removes one Card from this deck.
-    pop(): Card;
+  // Removes one Card from this deck.
+  pop(): CardData;
 
-    // Shuffles the cards in this deck.
-    shuffle(): void;
+  // Shuffles the cards in this deck.
+  shuffle(): void;
 }
